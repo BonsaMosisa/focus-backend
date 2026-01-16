40706699e457e5ba5c19b987564766fef63f40e0
BonsaMosisa <bonsa.adugnam@gmail.com>
2026-01-16 20:13:53 +0300

    galley controler fixed

--- src/controllers/galleryController.js
+++ src/controllers/galleryController.js
@@ -64,6 +64,7 @@ export const deleteImage = async (req, res) => {
 
     res.json({ message: "Deleted Successfully" });
   } catch (err) {
+    
     res.status(500).json({ message: err.message });
   }
 };
